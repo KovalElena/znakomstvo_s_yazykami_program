c80a953c65c966bf9b332e8e9f60f0c7361c1e3b
Elena <lelelena090287@gmail.com>
2023-05-23 11:06:59 +1000

    Added task08

---
+++ task08/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все четные числа от 1 до N.
+
+Console.Write("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 1)
+    Console.WriteLine("Некорректный ввод");
+else
+{
+    int i = 2;
+    for (i <= number)
+    {
+        if (i % 2 == 0)
+            Console.Write($"{i} ");
+        ++i;
+    }
+}
