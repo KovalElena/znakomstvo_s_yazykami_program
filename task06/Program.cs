59265586a896a1a434e6962e243bd23aecf969ca
Elena <lelelena090287@gmail.com>
2023-05-23 11:29:55 +1000

    added task06

--- task06/Program.cs
+++ task06/Program.cs
@@ -6,9 +6,9 @@ int number = Convert.ToInt32(Console.ReadLine());
 
 if (number % 2 == 0)
 {
-    Console.WriteLine(&"Число {number} является четным");
+    Console.WriteLine($"Число {number} является четным");
 }
 else
 {
-    Console.WriteLine(&"Число {number} не является четным");
+    Console.WriteLine($"Число {number} не является четным");
 }
