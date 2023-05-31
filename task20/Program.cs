41b6f05f926790839d2415ac38f93d65435c760e
Elena <lelelena090287@gmail.com>
2023-05-31 17:38:57 +1000

    Corrected mistakes

--- task20/Program.cs
+++ task20/Program.cs
@@ -18,7 +18,7 @@ double result = DistanceFromCoordinates(x1, y1, x2, y2);
 double resultRound = Math.Round(result, 2, MidpointRounding.ToZero);
 Console.WriteLine(resultRound);
 
-double DistanceFromCoordinates(int ax, int ay, int bx, int by);
+double DistanceFromCoordinates(int ax, int ay, int bx, int by)
 {
     double sumCathetus = (bx - ax) * (bx - ax) + (by - ay) * (by - ay);
     double d = Math.Sqrt(sumCathetus);
