e8a1df2d95dd1dae4db3904891dcbf5fba05fd48
Elena <lelelena090287@gmail.com>
2023-05-31 17:17:25 +1000

    Added task22

---
+++ task22/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число (N) и 
+// выдает таблицу квадратов чисел от 1 до N.
+
+Console.WriteLine("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+SqrTable(num);
+
+void SqrTable(int n)
+{
+    int count = 1;
+    while (count <= n) ;
+    {
+        Console.WriteLine($"{count} -> {count * count}");
+        count++;
+    }
+}
+
