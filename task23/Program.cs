a00fbcdaf7b040010b2134d8e89bf8ce6ececa18
Elena <lelelena090287@gmail.com>
2023-06-01 10:58:41 +1000

    added task23

---
+++ task23/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число (N) и 
+// выдает таблицу кубов чисел от 1 до N.
+
+
+Console.WriteLine("Введите натуральное число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+CubeTable(n);
+
+void CubeTable(int num)
+{
+    int i = 1;
+    while (i <= num)
+    {
+        Console.WriteLine($"{i,3 } -> {i * i * i, 4}");
+        i++;
+    }
+}
