7dceec57eb8a5d3aaf00a99ef15afc362ae31eff
Elena <lelelena090287@gmail.com>
2023-06-05 11:07:35 +1000

    Added task26

--- task24/Program.cs
+++ task24/Program.cs
@@ -2,14 +2,23 @@
 // и выдает сумму чисел от 1 до А.
 
 Console.WriteLine("Введите целое положительное число: ");
-int number = Convert.ToInt32(Console.ReadLine);
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number <= 0)
+{
+    Console.WriteLine("Введено неверное число");
+    return;
+}
+
+int sumNumbers = SumNumbers(number);
+Console.WriteLine($"Сумма числе от 1 до {number} равна {sumNumbers}");
 
 int SumNumbers(int num)
 {
-    int sum = 0;
-    for (int i = 1; i <= num; i++);
+    int sum = 1;
+    for (int i = 2; i <= num; i++)
     {
-        sum = sum + i; // sum += 1
+        sum = sum + i; // sum += i
     }
     return sum;
 }
---
+++ task26/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число и
+// выдает количство цифр в числе.
+
+Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int digitsInNumber = DigitsInNumber(number);
+
+Console.WriteLine($"{number} содержит {digitsInNumber} цифр(ы)");
+
+int DigitsInNumber(int num)
+{
+    int digits = 0;
+
+    if (num == 0) return 1;
+
+    while (num != 0)
+    {
+        digits++;
+        num /= 10;
+    }
+    return digits;
+}
+
+
+
