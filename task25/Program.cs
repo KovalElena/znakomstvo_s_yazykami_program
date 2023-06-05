c957a18192327887420a214c95ef553fdd4d4b17
Elena <lelelena090287@gmail.com>
2023-06-05 19:10:14 +1000

    added task25

---
+++ task25/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход два числа (А и В) и 
+// возводит число А в натуральную степень В.
+
+Console.WriteLine("Введите число А");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите целое положительное число В, в степень которого необходимо возвести число А");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberB >= 1)
+{
+    double aToThePowerOfb = AToThePowerOfB(numberA, numberB);
+    Console.WriteLine($"Число {numberA} в степени {numberB} равно {aToThePowerOfb}");
+}
+else
+{
+    Console.WriteLine("Введено неверное число В");
+}
+double AToThePowerOfB(int num1, int num2)
+{
+    double result = Math.Pow(num1, num2);
+    return result;
+}
