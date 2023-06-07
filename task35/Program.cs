a2eced19c0892e7786fa25fd926e163077eb7518
Elena <lelelena090287@gmail.com>
2023-06-07 16:27:45 +1000

    added task35, task37

---
+++ task35/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, 
+// значения которых лежат в отрезке [10,99].
+
+int[] CreateFillArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr, string sep = ",")
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}{sep} ");
+        else Console.Write($"{arr[i]}");
+    }
+}
+
+int CountTwoDig(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= 10 && arr[i] <= 99) count += 1;
+    }
+    return count;
+}
+
+int[] array = CreateFillArray(10, 0, 100);
+Console.Write("[");
+PrintArray(array, ";");
+Console.WriteLine("]");
+
+int countTwoDig = CountTwoDig(array);
+Console.WriteLine($" -> {countTwoDig}");
---
+++ task37/Program.cs
@@ -0,0 +1,47 @@
+﻿// Найдите произведение пар чисел в одномерном массиве.
+// Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+// Результат запишите в новом массиве.
+
+int[] CreateFillArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr, string sep = ",")
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}{sep} ");
+        else Console.Write($"{arr[i]}");
+    }
+}
+
+int[] MultiplyElement(int[] arr)
+{
+    int newLength = arr.Length / 2;
+    if (arr.Length % 2 == 1) newLength += 1;
+    int[] newArr = new int[newLength];
+
+    for (int i = 0; i < arr.Length / 2; i++)
+    {
+        newArr[i] = arr[i] * arr[arr.Length - 1 - i];
+    }
+    if (arr.Length % 2 == 1) newArr[arr.Length / 2] = arr[arr.Length / 2];
+    return newArr;
+}
+
+int[] array = CreateFillArray(7, 0, 10);
+Console.Write("[");
+PrintArray(array, ";");
+Console.WriteLine("]");
+
+int[] array2 = MultiplyElement(array);
+Console.Write("[");
+PrintArray(array2, ";");
+Console.WriteLine("]");
