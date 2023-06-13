f5eb78f3e09441999194ec247f98c65014e1395d
Elena <lelelena090287@gmail.com>
2023-06-13 18:27:57 +1000

    added task44 task45

---
+++ task44/Program.cs
@@ -0,0 +1,26 @@
+﻿// Не используя рекурсию,
+// выведите первые N чисел Фибоначчи.
+// Первые два числа Фибоначчи: 0 и 1.
+int[] CreateArrayFibonacci(int size, int first, int second)
+{
+    int[] array = new int[size];
+    array[0] = first;
+    array[1] = second;
+    for (int i = 2; i < array.Length; i++)
+    {
+        array[i] = array[i - 1] + array[i - 2];
+    }
+    return array;
+}
+
+void PrintArray(int[] arr, string sep = ",")
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}{sep} ");
+        else Console.Write($"{arr[i]}");
+    }
+}
+
+int[] array = CreateArrayFibonacci(10, 0, 1);
+PrintArray(array);
---
+++ task45/Program.cs
@@ -0,0 +1,43 @@
+﻿// Напишите программу, которая 
+// будет создавать копию заданного массива 
+// с помощью поэлементного копирования.
+
+int[] ArrayCopy(int[] arr)
+{
+    int[] newArr = new int[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        newArr[i] = arr[i];
+    }
+    return newArr;
+}
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr, string sep = ",")
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}{sep} ");
+        else Console.Write($"{arr[i]}");
+    }
+}
+
+int[] array = CreateArrayRndInt(8, 4, 10);
+Console.Write("[");
+PrintArray(array, ";");
+Console.WriteLine("]");
+int[] newArray = ArrayCopy(array);
+newArray[0] = 33;
+Console.Write("[");
+PrintArray(newArray, ";");
+Console.WriteLine("]");
