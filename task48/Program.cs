c34a1208e628851665232c762652d9d734eace09
Elena <lelelena090287@gmail.com>
2023-06-20 12:55:13 +1000

    added task48

---
+++ task48/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте двумерный массив размера m на n, 
+// каждый элемент в массиве находится по
+// формуле: Amn = m+n.
+// Выведите полученный массив на экран.
+
+int[,] CreateMatrixRndInt(int rows, int columns)
+{
+                            // 0      1
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+             matrix[i,j] = i + j;
+        }  
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j], 5}");
+        }
+       Console.WriteLine();
+    }
+}
+
+int[,] array2d = CreateMatrixRndInt(3, 4);
+PrintMatrix(array2d);
