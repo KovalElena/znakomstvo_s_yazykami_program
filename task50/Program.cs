f35966ad509e73af230eae8529dcc250e006fd71
Elena <lelelena090287@gmail.com>
2023-06-20 16:23:27 +1000

    added task50

---
+++ task50/Program.cs
@@ -0,0 +1,55 @@
+﻿// Напишите программу, которая на вход принимает 
+// позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента 
+// или же указание, что такого элемента нет.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    // 0      1
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine();
+    }
+}
+
+int ElemValue(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (numberA = i && numberB = j)
+            {
+                Console.WriteLine($"{matrix[i, j]}")
+            }
+        }
+        return matrix;
+    }
+}
+
+int[,] array2d = CreateMatrixRndInt(5, 5, -10, 10);
+PrintMatrix(array2d);
+ElemValue(array2d);
+Console.WriteLine($"Элемент на позиции ({numberA, numberB}) имеет значение ");
+
+
+
