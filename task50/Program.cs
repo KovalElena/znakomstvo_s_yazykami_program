2ef3932bac76e921a1c2710f5a4a3a7c3bad7d11
Elena <lelelena090287@gmail.com>
2023-06-23 16:55:03 +1000

    added task52

--- task50/Program.cs
+++ task50/Program.cs
@@ -30,40 +30,18 @@ void PrintMatrix(int[,] matrix)
     }
 }
 
-// int ElemValue(int numA, int numB)
-// {
-//     if (numA < 0 || numB < 0)
-//     {
-//         Console.WriteLine("Позиции элемента не могут быть отрицательными");
-//     }
-//     else if (numA > matrix.GetLength(0) || numB > matrix.GetLength(1))
-//     {
-//         Console.WriteLine("Такого элемента нет");
-//     }
-//     else
-//     {
-//         Console.WriteLine($"Элемент на позиции ({numA, numB}) имеет значение {matrix[i, j]}");
-//     }
-// }
-
-int ElemValue(int[,] matrix, int row, int column)
+bool IsElemExists(int[,] matrix, int row, int column)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            if (row > matrix.GetLength(0) || column > matrix.GetLength(1))
-            {
-                Console.WriteLine("Такого элемента нет");
-            }
-            else
-            {
-                Console.WriteLine($"Элемент имеет значение {matrix[i, j]}");
-            }
+            if (row >= 0 && row <= matrix.GetLength(0) && column >= 0 && column <= matrix.GetLength(1))
+                return true;
         }
     }
+    return false;
 }
-
 
 int[,] array2d = CreateMatrixRndInt(5, 5, -10, 10);
 PrintMatrix(array2d);
@@ -74,7 +52,7 @@ int Row = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите позицию элемента  в столбце: ");
 int Column = Convert.ToInt32(Console.ReadLine());
 
-int elemValue = ElemValue(array2d, Row, Column);
+Console.WriteLine(IsElemExists(array2d, Row, Column) ? $"Элемент имеет значение {array2d[Row, Column]}" : "Такого элемента нет");
 
 
 
---
+++ task52/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задайте двумерный массив из целых чисел.
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine();
+    }
+}
+
+void AverageOfEveryColumn(int[,] matrix)
+{
+    double sum = 0;
+    
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sum += matrix[i, j]; 
+        }
+        Console.Write($"Среднее арифметическое столбца {j} равно {sum / matrix.GetLength(0):F1} ");  
+    }
+}
+
+int[,] array2d = CreateMatrixRndInt(3, 3, 0, 10);
+PrintMatrix(array2d);
+
+AverageOfEveryColumn(array2d);
+
+
+
+
