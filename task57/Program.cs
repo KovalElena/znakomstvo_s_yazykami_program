eb44952130bdf7b4b23f25b00f794cabc3f39b68
Elena <lelelena090287@gmail.com>
2023-06-25 14:29:41 +1000

    added task57 task59

---
+++ task57/Program.cs
@@ -0,0 +1,85 @@
+﻿// Составить частотный словарь элементов двумерного массиваю
+// Частотный словарь содержит информацию от том, 
+// сколько раз встречается элемент входных данных.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    // 0      1
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine();
+    }
+}
+
+//  перевели двумерный массив в одномерный
+int[] MatrixToArray(int[,] matrix)
+{
+    int[] array = new int[matrix.Length];
+    int k = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            array[k] = matrix[i, j];
+            k++;
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[] arr, string sep = ",")
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}{sep} ");
+        else Console.Write($"{arr[i]}");
+    }
+}
+
+void CountFrequencies(int[] array)
+{
+    int currentNumber = array[0];
+    int count = 1;
+    
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] == currentNumber) count++;
+        else
+        {
+            Console.WriteLine($" число {currentNumber} встречается {count} раз");
+            currentNumber = array[i];
+            count = 1;
+        }
+    }
+    Console.WriteLine($" число {currentNumber} встречается {count} раз");
+}
+
+int[,] array2d = CreateMatrixRndInt(3, 4, 0, 10);
+int[] array = MatrixToArray(array2d);
+Array.Sort(array);
+
+PrintMatrix(array2d);
+Console.WriteLine();
+PrintArray(array);
+Console.WriteLine();
+CountFrequencies(array);
+
---
+++ task59/Program.cs
@@ -0,0 +1,84 @@
+﻿// Задайте двумерный массив из целых чиселю
+// Напишите программу, которая
+// удалит строку и столбец, на пересечении которых расположен 
+// наименьший элемент массива.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+                            // 0      1
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+             matrix[i,j] = rnd.Next(min, max + 1);
+        }
+       
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j], 5}");
+        }
+       Console.WriteLine();
+    }
+}
+
+int[,] IndexMinimalElement(int[,] matrix)
+{
+    int minElement = matrix[0,0];
+    int[] result = new int[2];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i,j] < minElement)
+            {
+                minElement = matrix[i,j];
+                result[0] = i;
+                result[1] = j;
+            }  
+        }
+       
+    }
+    return result;
+}
+
+int[,] RemoveRowColumn(int[,] matrix)
+{
+    int rows = matrix.GetLength(0) -1;
+    int columns = matrix.GetLength(1) -1;
+    int[,] newMatrix = new int[rows,columns];
+
+    int m = 0;
+    for (int i = 0; i < rows; i++)
+    {
+       if (m == matrix[0]) m++;
+
+       int n = 0;
+       for (int j = 0; j < columns; j++)
+       {
+        if (n == matrix[1]) n++;
+       newMatrix[i,j] = matrix[m,n];
+       n++;
+       }
+       m++;
+    }
+    return newMatrix;
+}
+
+
+int[,] array2d = CreateMatrixRndInt(3, 4, -100, 100);
+PrintMatrix(array2d);
+Console.WriteLine();
+
+int[] indexMinimalElement = IndexMinimalElement(matrix);
+int[,] newMatrix = RemoveRowColumn(IAsyncResult, matrix);
+PrintMatrix(newMatrix);
